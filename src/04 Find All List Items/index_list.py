848f5fe565216ed4bf50e12ddd504bd101c95fe2
carobson652 <carobson652@yahoo.com>
2025-01-14 14:43:40 +0000

    More solutions

--- src/04 Find All List Items/index_list.py
+++ src/04 Find All List Items/index_list.py
@@ -1,26 +1,15 @@
 def index_all(theList, item):
   retList = []
-  if isinstance(item, list ):
-    for subItem in item:
-      matches = index_all(theList, subItem)
-      for match in matches:
-        retList.append(match)
-  else:
-    for i in range ( 0, len(theList) ):
-      if isinstance(theList[i], list):
-        for j in range (0, len(theList[i])):
-          if isinstance(theList[i][j], list ):
-            matches = index_all(theList[i][j], item)
-            for match in matches:
-              retList.append([ i, j, match ])
-          else:
-            if item == theList[i][j]:
-              retList.append([i, j])
-      else:
-        if item == theList[i]:
-          retList.append(i)
+  for index,value in enumerate(theList):
+    if value == item:
+      retList.append([index])
+    else:
+      if isinstance(value, list):
+        for subIndex in index_all( value, item ):
+          retList.append( [index] + subIndex )
   return retList
        
 example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
 print ( str ( index_all ( example, 2) ))
+print ("")
 print ( str ( index_all ( example, [ 1, 2, 3]) ))
